91067126090a5de7fc9de93d95cc884bf06cc94f
bastndev <gohit24@gmail.com>
2023-06-22 11:06:14 -0500

    :arrow_up: Class `207`

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,10 +8,22 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AuthBackground(
-        child: Container(
-          width: double.infinity,
-          height: 300,
-          color: Colors.red,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 250),
+              CardContainer(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Text('Login', style: Theme.of(context).textTheme.headlineMedium),
+                    const SizedBox(height: 30),
+                    const Text('Form')
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,4 @@
+export 'package:app_products/widgets/card_container.dart';
+
 export 'package:app_products/widgets/auth_background.dart';
 
