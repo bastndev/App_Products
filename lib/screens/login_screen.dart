88dd19b344312f7f71a8abfcba69ac27553f0854
bastndev <gohit24@gmail.com>
2023-06-22 11:38:19 -0500

    :arrow_up: class `208`

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,14 +16,58 @@ class LoginScreen extends StatelessWidget {
                 child: Column(
                   children: [
                     const SizedBox(height: 10),
-                    Text('Login', style: Theme.of(context).textTheme.headlineMedium),
-                    const SizedBox(height: 30),
-                    const Text('Form')
+                    Text('Login',
+                        style: Theme.of(context).textTheme.headlineMedium),
+                    const SizedBox(height: 10),
+                    // const Text('Form'),
+                    const _LoginForm(),
                   ],
                 ),
               ),
+              const SizedBox(height: 50),
+              const Text('Sign up')
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //TODO: mantener la referencia KEY
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.purple,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2,
+                  ),
+                ),
+                labelText: 'Email',
+                hintText: 'example@gmail.com',
+                labelStyle: TextStyle(
+                  color: Colors.grey
+                ),
+                prefixIcon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+              ),
+            ),
+          ],
         ),
       ),
     );
