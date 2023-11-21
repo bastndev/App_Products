8f3868968077104195fa8308dcb39b283b01d0f9
bastndev <gohit24@gmail.com>
2023-11-20 19:05:14 -0500

    :arrow_up: Update `new`

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,7 @@
+export  'package:app_products/widgets/product_card.dart';
+
 export 'package:app_products/widgets/card_container.dart';
 
 export 'package:app_products/widgets/auth_background.dart';
+
 
