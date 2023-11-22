784ee602361cd3b1cc23eddb923d6a928c797d1c
bastndev <gohit24@gmail.com>
2023-11-22 10:28:31 -0500

    :arrow_up: Update `new`

---
+++ lib/widgets/product_image.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductImage extends StatelessWidget {
+  const ProductImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,7 +1,7 @@
-export  'package:app_products/widgets/product_card.dart';
+export 'package:app_products/widgets/product_image.dart';
+
+export 'package:app_products/widgets/product_card.dart';
 
 export 'package:app_products/widgets/card_container.dart';
 
 export 'package:app_products/widgets/auth_background.dart';
-
-
