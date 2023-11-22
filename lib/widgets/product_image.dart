96a06657f423eeb8627be1ced217f50381bc1740
bastndev <gohit24@gmail.com>
2023-11-22 11:13:41 -0500

    :arrow_up: Update `new`

--- lib/widgets/product_image.dart
+++ lib/widgets/product_image.dart
@@ -11,6 +11,14 @@ class ProductImage extends StatelessWidget {
         decoration: _buildBoxDecoration(),
         width: double.infinity,
         height: 370,
+        child: const ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+          child: FadeInImage(
+            image: NetworkImage('http://via.placeholder.com/300x300/black'),
+            placeholder: AssetImage('assets/image/jar-loading.gif'),
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
     );
   }
@@ -20,7 +28,7 @@ class ProductImage extends StatelessWidget {
         borderRadius: const BorderRadius.all(Radius.circular(30)),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.19),
+            color: Colors.black.withOpacity(0.4),
             blurRadius: 10,
             offset: const Offset(0, 5),
           ),
