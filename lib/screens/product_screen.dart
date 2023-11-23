934267214f530a73b6fcedfa904be2cbfe756494
bastndev <gohit24@gmail.com>
2023-11-23 09:18:51 -0500

    :arrow_up: Update `new`

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -6,16 +6,27 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             Stack(
               children: [
-                ProductImage()
+                const ProductImage(),
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
               ],
             )
-
           ],
         ),
       ),
