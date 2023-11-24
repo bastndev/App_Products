9471b21dddcf1e968028486018b7271904d42be2
bastndev <gohit24@gmail.com>
2023-11-24 09:43:36 -0500

    :arrow_up: Update `new`

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,17 @@
-import 'package:app_products/widgets/product_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:app_products/services/services.dart';
+import 'package:app_products/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    final productService = Provider.of<ProductService>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Products'),
